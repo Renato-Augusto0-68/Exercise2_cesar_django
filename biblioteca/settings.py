6b6bbee0ff68ac0d14c5bdf6d99719febafd016c
Rutheford <renatoaugusto9731@gmail.com>
2025-09-15 11:32:21 -0300

    models creation

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'proj_bib.apps.ProjBibConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ proj_bib/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+import datetime
+from django.utils import timezone
+class Usuario(models.Model):
+    Nome = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=200,unique=True)
+    data_criacao = models.DateTimeField(timezone.now())
