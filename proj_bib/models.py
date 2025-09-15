1382e00d7791f11b79cd39542db389f0c8054158
Rutheford <renatoaugusto9731@gmail.com>
2025-09-15 11:50:30 -0300

    models.part2

--- proj_bib/models.py
+++ proj_bib/models.py
@@ -9,4 +9,14 @@ class Usuario(models.Model):
     data_criacao = models.DateTimeField(timezone.now())
 
     def __str__(self):
-        return self.Nome
+        return self.Nome,self.data_criacao
+
+class emprestimo(models.Model):
+    user=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    livro=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    data_emprest=models.DeteField()
+    data_devol=models.DateField(null=True,blank=True)
+    status=models.CharField(max_length=20,choices=[('Aberto,','Aberto'),('Devolvido','Devolvido')])
+
+    def __str__(self):
+        return  f"{self.user}->{self.livro}"
